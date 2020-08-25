99e34169858519bef6d0a91ef1cb259241ce4f60
Mikel Zhobro <mikel.zhobro@iav.de>
2020-08-25 17:04:11 +0200

    added Ctra3d

--- src/include/motion_model/motion_model.h
+++ src/include/motion_model/motion_model.h
@@ -34,7 +34,9 @@
 // 	 	 it is better done in KalmanWrapper after both observation_step and prediction_step
 namespace iav{ namespace state_predictor { namespace motion_model {
 	
-	// @brief Type trait base class -> called primary template class
+	/**
+	* @brief Type trait base class -> called primary template class
+	*/
 	template<int num_states, typename T = double>
 	class MotionModel
 	{
@@ -44,13 +46,15 @@ namespace iav{ namespace state_predictor { namespace motion_model {
 		using StateMatrix = Eigen::Matrix<T, num_states, num_states>;
 	};
 
-	// @brief ctra2D: Constant turning rate and acceleration
+	/**
+	* @brief ctra2D: Constant turning rate and acceleration
+	*/
 	template<typename T = double>
 	class MotionModelCtra2D : public MotionModel<8, T>
 	{
 	public:		
 		/**
-		* This state gives named handles for state indexing
+		* @brief This state gives named handles for state indexing
 		*/
 		struct States
 		{
@@ -173,13 +177,15 @@ namespace iav{ namespace state_predictor { namespace motion_model {
 		}
 	};
 
-	// @brief ctrv2D: Constant turning rate and velocity
+	/**
+	* @brief ctrv2D: Constant turning rate and velocity
+	*/
 	template<typename T = double>
 	class MotionModelCtrv2D : public MotionModel<6, T>
 	{
 	public:
 		/**
-		* This state gives named handles for state indexing
+		* @brief This state gives named handles for state indexing
 		*/
 		struct States
 		{
@@ -277,6 +283,217 @@ namespace iav{ namespace state_predictor { namespace motion_model {
 		}
 	};
 
+	/**
+	* @brief ctra3D: Constant turning rate and acceleration
+	*/
+	template<typename T = double>
+	class MotionModelCtra3D : public MotionModel<15, T>
+	{
+	public:		
+		/**
+		* @brief This state gives named handles for state indexing
+		*/
+		struct States
+		{
+			static constexpr int X = 0U;  ///< index of x position
+			static constexpr int Y = 1U;  ///< index of y position
+			static constexpr int Z = 2U;  ///< index of z position
+			static constexpr int ROLL= 3U;  ///< index of roll position
+			static constexpr int PITCH= 4U;  ///< index of pitch position
+			static constexpr int YAW= 5U;  ///< index of yaw position
+			static constexpr int V_X = 6U;  ///< index of x velocity
+			static constexpr int V_Y = 7U;  ///< index of y velocity
+			static constexpr int V_Z = 8U;  ///< index of z velocity
+			static constexpr int V_ROLL = 9U;  ///< index of roll velocity
+			static constexpr int V_PITCH= 10U;  ///< index of pitch velocity
+			static constexpr int V_YAW= 11U;  ///< index of yaw velocity
+			static constexpr int A_X = 12U;  ///< index of x acceleration
+			static constexpr int A_Y = 13U;  ///< index of y acceleration
+			static constexpr int A_Z = 14U;  ///< index of z acceleration
+
+		};  // struct States
+
+	public:		
+		using StateVector = typename MotionModel::StateVector;
+		using StateMatrix = typename MotionModel::StateMatrix;
+		
+		/**
+		 * @brief Ctra3D: Method that computes the Jacobian and performs a predict step
+		 *		  in the right order. It leverages the computed Jacobian to save some computations too.
+		 * @param[inout] jacobi - Matrix to save Jacobian into
+		 * @param[inout] state - The actual state to be used to compute the Jacobian and be updated later
+		 * @param[in] dt - The time-difference for which the prediction has to be performed
+		 * @return the bounded value
+		 */
+		static void compute_jacobian_and_predict(StateMatrix& jacobi, StateVector& state, const tTime& dt)
+		{
+			compute_jacobian(jacobi, state, dt);
+			predict(state, jacobi, dt);
+		}
+
+		/**
+		 * @brief Ctra3D: Method that computes the Jacobian
+		 * 		  in the right order. It leverages the computed Jacobian to save some computations too.
+		 * @param[inout] jacobi - Matrix to save Jacobian into
+		 * @param[in] state - The actual state to be used to compute the Jacobian
+		 * @param[in] dt - The time-difference for which the prediction has to be performed
+		 * @return the bounded value
+		 */
+		//TO_DO
+		static void compute_jacobian(StateMatrix& jacobi, const StateVector& state, const tTime& dt)
+		{
+			T dt_2 = 0.5 * dt;
+			T cr = cos(state[States::ROLL]);
+			T sr = sin(state[States::ROLL]);
+
+			T cp = cos(state[States::PITCH]);
+			T sp = sin(state[States::PITCH]);
+			T cpi = 1.0f/cp;
+			T tp = sp * cpi;
+
+			T cy = cos(state[States::YAW]);
+			T sy = sin(state[States::YAW]);
+
+			StateMatrix jacobi;
+			jacobi.setIdentity();
+
+			// The Linear part
+			jacobi(States::X, States::V_X) = dt * cy * cp;
+			jacobi(States::X, States::V_Y) = dt * (cy*sp*sr - sy*cr);
+			jacobi(States::X, States::V_Z) = dt * (cy*sp*cr + sy*sr);
+			jacobi(States::X, States::A_X) = dt_2 * cy * cp;
+			jacobi(States::X, States::A_Y) = dt_2 * (cy*sp*sr - sy*cr);
+			jacobi(States::X, States::A_Z) = dt_2 * (cy*sp*cr + sy*sr);
+
+			jacobi(States::Y, States::V_X) = dt * vx * sy * cp;
+			jacobi(States::Y, States::V_Y) = dt * (sy*sp*sr+cy*cr);
+			jacobi(States::Y, States::V_Z) = dt * (sy*sp*cr-cy*sr);
+			jacobi(States::Y, States::A_X) = dt_2 * sy * cp;
+			jacobi(States::Y, States::A_Y) = dt_2 * (sy*sp*sr + cy*cr);
+			jacobi(States::Y, States::A_Z) = dt_2 * (sy*sp*cr - cy*sr);
+
+			jacobi(States::Z, States::V_X) = -dt * sp;
+			jacobi(States::Z, States::V_Y) = dt * cp * sr;
+			jacobi(States::Z, States::V_Z) = dt * cp * cr;
+			jacobi(States::Z, States::A_X) = dt_2 * sp;
+			jacobi(States::Z, States::A_Y) = dt_2 * cp * sr;
+			jacobi(States::Z, States::A_Z) = dt_2 * cp * cr;
+
+			jacobi(States::ROLL, States::V_ROLL) = dt;
+			jacobi(States::ROLL, States::V_PITCH) = dt * sr * tp;
+			jacobi(States::ROLL, States::V_YAW) = dt * cr * tp;
+			jacobi(States::PITCH, States::V_PITCH) = dt * cr;
+			jacobi(States::PITCH, States::V_YAW) = -dt * sr;
+			jacobi(States::YAW, States::V_PITCH) = dt * sr * cpi;
+			jacobi(States::YAW, States::V_YAW) = dt * cr * cpi;
+
+			jacobi(States::V_X, States::A_X) = dt;
+			jacobi(States::V_Y, States::A_Y) = dt;
+			jacobi(States::V_Z, States::A_Y) = dt;
+
+			//TO_DO 
+			//The nonlinear part
+			jacobi(States::X, States::ROLL) = 0;
+			jacobi(States::X, States::PITCH) = 0;
+			jacobi(States::X, States::YAW) = 0;
+			jacobi(States::Y, States::ROLL) = 0;
+			jacobi(States::Y, States::PITCH) = 0;
+			jacobi(States::Y, States::YAW) = 0;
+			jacobi(States::ROLL, States::ROLL) = 0;
+			jacobi(States::ROLL, States::PITCH) = 0;
+			jacobi(States::PITCH, States::PITCH) = 0;
+			jacobi(States::YAW, States::ROLL) = 0;
+			jacobi(States::YAW, States::PITCH) = 0;
+		}
+
+		/**
+		 * @brief Ctra3D: Method that updates the current state with a given motion. It is called after
+		 * 		  compute_jacobian() in order to make sure that the state is not updated before 
+		 * 		  the Jacobian is computed. It is normally followed by an observation update.
+		 * 		  It is faster than the other predict function as it leverages the computed Jacobian 
+		 * 		  to save some computations!
+		 * @param[inout] jacobi - Matrix to save Jacobian into
+		 * @param[in] state - The actual state to be used to compute the Jacobian
+		 * @param[in] dt - The time-difference for which the prediction has to be performed
+		 * @return the bounded value
+		 */
+		static void predict(StateVector& state, const StateMatrix& jacobi, const tTime& dt)
+		{
+			StateMatrix transform_matrix = jacobi;
+			transform_matrix(States::X, States::ROLL) = 0;
+			transform_matrix(States::X, States::PITCH) = 0;
+			transform_matrix(States::X, States::YAW) = 0;
+			transform_matrix(States::Y, States::ROLL) = 0;
+			transform_matrix(States::Y, States::PITCH) = 0;
+			transform_matrix(States::Y, States::YAW) = 0;
+			transform_matrix(States::ROLL, States::ROLL) = 0;
+			transform_matrix(States::ROLL, States::PITCH) = 0;
+			transform_matrix(States::PITCH, States::PITCH) = 0;
+			transform_matrix(States::YAW, States::ROLL) = 0;
+			transform_matrix(States::YAW, States::PITCH) = 0;
+			state = transform_matrix * state;
+		}
+
+		/**
+		 * @brief Ctra3D: Method that updates the current state with a given motion. It is called after
+		 * 		  compute_jacobian() in order to make sure that the state is not updated before 
+		 * 		  the Jacobian is computed. It is normally followed by an observation update.
+		 * @param[inout] jacobi - Matrix to save Jacobian into
+		 * @param[in] state - The actual state to be used to compute the Jacobian
+		 * @param[in] dt - The time-difference for which the prediction has to be performed
+		 * @return the bounded value
+		 */
+		static void predict(StateVector& state, const tTime& dt)
+		{
+			T dt_2 = 0.5 * dt;
+			T cr = cos(state[States::ROLL]);
+			T sr = sin(state[States::ROLL]);
+
+			T cp = cos(state[States::PITCH]);
+			T sp = sin(state[States::PITCH]);
+			T cpi = 1.0f/cp;
+			T tp = sp * cpi;
+
+			T cy = cos(state[States::YAW]);
+			T sy = sin(state[States::YAW]);
+
+			StateMatrix transform_matrix;
+			transform_matrix.setIdentity();
+			transform_matrix(States::X, States::V_X) = dt * cy * cp;
+			transform_matrix(States::X, States::V_Y) = dt * (cy*sp*sr - sy*cr);
+			transform_matrix(States::X, States::V_Z) = dt * (cy*sp*cr + sy*sr);
+			transform_matrix(States::X, States::A_X) = dt_2 * cy * cp;
+			transform_matrix(States::X, States::A_Y) = dt_2 * (cy*sp*sr - sy*cr);
+			transform_matrix(States::X, States::A_Z) = dt_2 * (cy*sp*cr + sy*sr);
+
+			transform_matrix(States::Y, States::V_X) = dt * vx * sy * cp;
+			transform_matrix(States::Y, States::V_Y) = dt * (sy*sp*sr+cy*cr);
+			transform_matrix(States::Y, States::V_Z) = dt * (sy*sp*cr-cy*sr);
+			transform_matrix(States::Y, States::A_X) = dt_2 * sy * cp;
+			transform_matrix(States::Y, States::A_Y) = dt_2 * (sy*sp*sr + cy*cr);
+			transform_matrix(States::Y, States::A_Z) = dt_2 * (sy*sp*cr - cy*sr);
+
+			transform_matrix(States::Z, States::V_X) = -dt * sp;
+			transform_matrix(States::Z, States::V_Y) = dt * cp * sr;
+			transform_matrix(States::Z, States::V_Z) = dt * cp * cr;
+			transform_matrix(States::Z, States::A_X) = dt_2 * sp;
+			transform_matrix(States::Z, States::A_Y) = dt_2 * cp * sr;
+			transform_matrix(States::Z, States::A_Z) = dt_2 * cp * cr;
+
+			transform_matrix(States::ROLL, States::V_ROLL) = dt;
+			transform_matrix(States::ROLL, States::V_PITCH) = dt * sr * tp;
+			transform_matrix(States::ROLL, States::V_YAW) = dt * cr * tp;
+			transform_matrix(States::PITCH, States::V_PITCH) = dt * cr;
+			transform_matrix(States::PITCH, States::V_YAW) = -dt * sr;
+			transform_matrix(States::YAW, States::V_PITCH) = dt * sr * cpi;
+			transform_matrix(States::YAW, States::V_YAW) = dt * cr * cpi;
+
+			transform_matrix(States::V_X, States::A_X) = dt;
+			transform_matrix(States::V_Y, States::A_Y) = dt;
+			transform_matrix(States::V_Z, States::A_Y) = dt;
+			state = transform_matrix * state;
+		}
+	};
 	using Ctrv2D = MotionModelCtrv2D<double>;
 	using Ctra2D = MotionModelCtra2D<double>;
 } // end namespace motion_model 
---
+++ test/tst_motion_model_ctra3d.cpp
@@ -0,0 +1,131 @@
+/*! \file
+*
+* Copyright (c) 1983-2020 IAV GmbH. All rights reserved.
+*
+* \authors Mikel Zhobro, Robert Treiber IAV GmbH
+*
+* Correspondence should be directed to IAV.
+*
+* IAV GmbH\n
+* Carnotstraße 1\n
+* 10587 Berlin
+*
+* GERMANY
+*
+* \note
+* Neither IAV GmbH nor the authors admit liability
+* nor provide any warranty for any of this software.
+* Until the distribution is granted by IAV GmbH
+* this source code is under non disclosure and must
+* only be used within projects with controlling
+* interest of IAV GmbH.
+*/
+#pragma once
+
+#include <gtest/gtest.h>
+#include "TestHelper.h"
+#include <motion_model/motion_model.h>
+
+
+
+TEST(TestCtra, PredictAndJacobi)
+{
+    const iav::state_predictor::motion_model::Ctra2D model;
+    using States = iav::state_predictor::motion_model::Ctra2D::States;
+    using CtrvState = iav::state_predictor::motion_model::Ctra2D::StateVector;
+    using CtrvMatris = iav::state_predictor::motion_model::Ctra2D::StateMatrix;
+
+    // dts
+    const double seconds = 1;
+    const double milliseconds_10 = 0.01;
+    const double milliseconds_100 = 0.1;
+
+    // set state
+    const double vx = -1.0F;
+    const double vy = 2.0F;
+    const double ax = -0.5F;
+    const double ay = 0.5F;
+    const double yaw = iav::state_predictor::PI/3;
+
+    CtrvState x;
+    x << 0.0F, 0.0F, yaw, vx, vy, 0, ax, ay;
+
+    // predict
+    //define lambda for quick testing
+    auto test_predict = [=](CtrvState x, double dt)
+    {
+        std::cout << "Ctra: Testing Predict with dt=" << dt*1000 << "ms.\n";
+        model.predict(x,dt);
+        CtrvState x1;
+        x1 << vx*dt*cos(yaw) - vy*dt*sin(yaw) + 0.5*dt*dt*ax*cos(yaw) - 0.5*dt*dt*ay*sin(yaw),
+            vx*dt*sin(yaw) + vy*dt*cos(yaw) + 0.5*dt*dt*ax * sin(yaw) +  0.5*dt*dt*ay*cos(yaw),
+            yaw,
+            vx + dt*ax,
+            vy + dt*ay,
+            0,
+            ax,
+            ay;
+        ASSERT_MATRIX_NEAR(x,x1,1e-7);
+    };
+    test_predict(x, seconds);
+    test_predict(x, milliseconds_10);
+    test_predict(x, milliseconds_100);
+
+    // jacobi
+    auto test_jacobi= [=](CtrvState x, double dt)
+    {
+        std::cout << "Ctra: Testing Jacobi with dt=" << dt*1000 << "ms.\n";
+        CtrvMatris jacobi;
+        model.compute_jacobian(jacobi, x, dt);
+
+        CtrvMatris jacobi1;
+        double dt_c_yaw = dt*cos(yaw);
+        double dt_s_yaw = dt*sin(yaw);
+        double dt_dt_c_yaw_05 = dt_c_yaw * dt * 0.5;
+        double dt_dt_s_yaw_05 = dt_s_yaw * dt * 0.5;
+
+        jacobi1.setIdentity();
+        jacobi1(States::X, States::V_X) = dt_c_yaw;
+        jacobi1(States::X, States::V_Y) = -dt_s_yaw;
+        jacobi1(States::X, States::YAW) = -dt_s_yaw * vx - dt_c_yaw * vy
+                                        - dt_dt_s_yaw_05 * ax - dt_dt_c_yaw_05 * ay;
+        jacobi1(States::X, States::A_X) = dt_dt_c_yaw_05;
+        jacobi1(States::X, States::A_Y) = -dt_dt_s_yaw_05;
+        jacobi1(States::V_X, States::A_X) = dt;
+
+        jacobi1(States::Y, States::V_X) = dt_s_yaw;
+        jacobi1(States::Y, States::V_Y) = dt_c_yaw;
+        jacobi1(States::Y, States::YAW) = dt_c_yaw * vx - dt_s_yaw * vy
+                                        + dt_dt_c_yaw_05 * ax - dt_dt_s_yaw_05 * ay;
+        jacobi1(States::Y, States::A_X) = dt_dt_s_yaw_05;
+        jacobi1(States::Y, States::A_Y) = dt_dt_c_yaw_05;
+        jacobi1(States::V_Y, States::A_Y) = dt;
+        jacobi1(States::YAW, States::V_YAW) = dt;
+
+        ASSERT_MATRIX_NEAR(jacobi, jacobi1, 1e-7);
+    };
+    test_jacobi(x, seconds);
+    test_jacobi(x, milliseconds_10);
+    test_jacobi(x, milliseconds_100);
+
+    // jacobi & predict
+    auto test_jacobi_and_predict= [=](CtrvState x, double dt)
+    {
+        std::cout << "Ctra: Testing JacobiAndPredict with dt=" << dt*1000 << "ms.\n";
+        // since we tested above we can use the functions predict() and compute_jacobian()
+        CtrvMatris jacobi1;
+        CtrvState x1 = x;
+        model.predict(x1, dt);
+        model.compute_jacobian(jacobi1, x, dt);
+        
+        CtrvMatris jacobi;
+        model.compute_jacobian_and_predict(jacobi, x, dt);
+
+        // ASSERT_EQ(x(3), x1(3));
+        ASSERT_MATRIX_NEAR(x, x1, 1e-5);
+        ASSERT_MATRIX_NEAR(jacobi, jacobi1, 1e-7);
+    };
+    test_jacobi_and_predict(x, seconds);
+    test_jacobi_and_predict(x, milliseconds_10);
+    test_jacobi_and_predict(x, milliseconds_100);
+};
