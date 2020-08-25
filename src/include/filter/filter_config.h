71cdfeff061ed02000a8aec0bbc0fc2f0984d8e5
Mikel Zhobro <mikel.zhobro@iav.de>
2020-08-25 14:42:40 +0200

    added license

--- src/include/filter/filter_config.h
+++ src/include/filter/filter_config.h
@@ -1,3 +1,26 @@
+/*! \file
+*
+* Copyright (c) 1983-2020 IAV GmbH. All rights reserved.
+*
+* \authors Mikel Zhobro, Robert Treiber IAV GmbH
+*
+* Correspondence should be directed to IAV.
+*
+* IAV GmbH\n
+* Carnotstraße 1\n
+* 10587 Berlin
+*
+* GERMANY
+*
+* \note
+* Neither IAV GmbH nor the authors admit liability
+* nor provide any warranty for any of this software.
+* Until the distribution is granted by IAV GmbH
+* this source code is under non disclosure and must
+* only be used within projects with controlling
+* interest of IAV GmbH.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <map>
