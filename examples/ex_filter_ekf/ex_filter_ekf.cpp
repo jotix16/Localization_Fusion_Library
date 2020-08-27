6dcad883195461689782b1c3b1454189f7eb5ba1
Mikel Zhobro <mikel.zhobro@iav.de>
2020-08-27 18:47:21 +0200

    cleaning. (ground truth not ok)

--- examples/ex_filter_ekf/ex_filter_ekf.cpp
+++ examples/ex_filter_ekf/ex_filter_ekf.cpp
@@ -13,35 +13,6 @@ using namespace Eigen;
 
 namespace plt = matplotlibcpp;
 
-// int main()
-// {
-//     // Prepare data.
-//     int n = 5000;
-//     std::vector<double> x(n), y(n), z(n), w(n,2);
-//     for(int i=0; i<n; ++i) {
-//         x.at(i) = i*i;
-//         y.at(i) = sin(2*3.14*i/360.0);
-//         z.at(i) = log(i);
-//     }
-
-//     // Set the size of output image to 1200x780 pixels
-//     plt::figure_size(1200, 780);
-//     // Plot line from given x and y data. Color is selected automatically.
-//     plt::plot(x, y);
-//     // Plot a red dashed line from given x and y data.
-//     plt::plot(x, w,"r--");
-//     // Plot a line whose name will show up as "log(x)" in the legend.
-//     plt::named_plot("log(x)", x, z);
-//     // Set x-axis to interval [0,1000000]
-//     plt::xlim(0, 1000*1000);
-//     // Add graph title
-//     plt::title("Sample figure");
-//     // Enable legend.
-//     plt::legend();
-//     // Save the image (file format is determined by the extension)
-//     plt::show();
-//     plt::save("./basic.png");
-// }
 // noise generator
 std::random_device rd;
 std::mt19937 gen(rd());
@@ -50,7 +21,6 @@ double normal_dist()
 {
     return d(gen);
 }
-
 
 struct MotionModel
 {
@@ -80,7 +50,8 @@ struct MotionModel
         state(0,0) += dt * state(1,0);
     }
 
-    // Simulates the radar signal retuns from an object flying at a constant altitude and velocity in 1d
+    // The two functions below simulate the radar signal returns from an object flying 
+    // at a constant altitude and velocity in 2d.
     static StateVector simulate(StateVector& state, const tTime& dt)
     {
         // update state after adding process noise
@@ -96,7 +67,6 @@ struct MotionModel
         return meas + err ;
     }
 };
-
 
 struct MeasurementModel
 {
@@ -130,6 +100,106 @@ struct MeasurementModel
 
 using Ctra_EKF3D = iav::state_predictor::filter::FilterEkf<MotionModel, 3, double>;
 
+void plot(std::vector<double>x, std::vector<double> y_ground_truth, std::vector<double> y_estimated, bool save, char* title= "My Title")
+{
+    // Set the size of output image to 1200x780 pixels
+    plt::figure_size(1200, 780);
+    // Plot line from given x and y data. Color is selected automatically.
+    plt::named_plot("Filter estimation", x, y_estimated);
+    // Plot a red dashed line from given x and y data.
+    plt::named_plot("Ground truth", x, y_ground_truth, "r--");
+    // Add graph title
+    plt::title(title);
+    // Enable legend.
+    plt::legend();
+    // Save the image (file format is determined by the extension)
+    plt::show();
+    if(save)  plt::save("./basic.png");
+
+}
+
+
+int main()
+{
+    using StateVector = MotionModel::StateVector;
+    using StateMatrix = MotionModel::StateMatrix;
+    using Measurement = MeasurementModel::MeasurementVector;
+    using JacobiMatrix = MeasurementModel::JacobiMatrix;
+
+    // time step
+    double dt = 0.05;
+    // state inits
+    StateVector state0, state;
+    double x = 0.0, vx = 100, y = 1000.0;
+    state << x, vx, y; // the real state
+    state0 << x-50, vx+100, y+1000; // init state
+
+    // COVARIANCES
+    // State covariance init
+    StateMatrix P0;
+    P0.setIdentity();
+    P0 *= 50.0;
+
+    // Process noise covariance
+    StateMatrix Q;
+    Q.setIdentity();
+    // Q *= 0.1;
+    Q(0,0) *= 0.25 * std::pow(dt,4) * 0.1;
+    Q(1,1) *= 0.5 * std::pow(dt,2) * 0.1;
+    Q(0,1) *= std::pow(dt,3) * 0.1;
+    Q(1,0) *= 0.5 * std::pow(dt,3) * 0.1;
+
+    Q(2,2) *= 0.1;
+    
+    // Measurement noise covariance
+    double range_std = 1;
+    MatrixXd R(1,1);
+    R.setIdentity();
+    R *= range_std*range_std;
+
+    // Kalman Filter
+    Ctra_EKF3D ekf;
+    ekf.reset(state0, P0, Q);
+
+    std::vector<double> ground_truth_x, ground_truth_vx, ground_truth_y, filter_estimate_x, filter_estimate_vx, filter_estimate_y, times;
+    StateVector state_temp;
+    std::vector<StateVector> ground_truth, filter_estimate;
+    Measurement z = MeasurementModel::h(state, dt);
+    JacobiMatrix H = MeasurementModel::H(state, dt);
+
+    int timesteps = 60.0/dt;
+    for (int i = 0; i < timesteps; i++)
+    {
+        times.push_back(i);
+        state_temp = MotionModel::simulate(state, dt);
+        ground_truth.push_back(state_temp);
+        ground_truth_x.push_back(state_temp[0]);
+        ground_truth_vx.push_back(state_temp[1]);
+        ground_truth_y.push_back(state_temp[2]);
+
+        z = MeasurementModel::h(state, dt);
+        H = MeasurementModel::H(state, dt);
+        ekf.observation_update(z, H, R, 1000000);
+        filter_estimate.push_back(ekf.get_state());
+        
+        state_temp = ekf.get_state();
+        filter_estimate_x.push_back(state_temp[0]);
+        filter_estimate_vx.push_back(state_temp[1]);
+        filter_estimate_y.push_back(state_temp[2]);
+        ekf.temporal_update(dt);
+    }
+
+    // PLOT
+    plot(times, ground_truth_x, filter_estimate_x, false, "Airplane x");
+    plot(times, ground_truth_y, filter_estimate_y, false, "Airplane y");
+    
+    std::cout<<"Ground truth state is: \n"<< state << "\n";
+    std::cout<<"Estimated state is: \n"<< ekf.get_state() << "\n";
+
+    return 0;
+}
+
+
 void check_normal_dist()
 {
   // check the noise generator 
@@ -148,146 +218,4 @@ void check_normal_dist()
     std::cout << i << "-" << (i+1) << ": ";
     std::cout << std::string(p[i]*nstars/nrolls,'*') << std::endl;
   }
-}
-
-
-
-int main()
-{
-    using StateVector = MotionModel::StateVector;
-    using StateMatrix = MotionModel::StateMatrix;
-    using Measurement = MeasurementModel::MeasurementVector;
-    using JacobiMatrix = MeasurementModel::JacobiMatrix;
-
-    // time step
-    double dt = 0.05;
-    // state inits
-    StateVector state0, state;
-    double x = 0.0, vx = 100, y = 1000.0;
-    state << x, vx, y; // the real state
-    state0 << x-5, vx-10, y+100; // init state
-
-    // COVARIANCES
-    // State covariance init
-    StateMatrix P0;
-    P0.setIdentity();
-    P0 *= 50.0;
-
-    // Process noise covariance
-    StateMatrix Q;
-    Q.setIdentity();
-    Q *= 0.1;
-    // Q(0,0) *= 0.25 * std::pow(dt,4) * 0.1;
-    // Q(1,1) *= 0.5 * std::pow(dt,2) * 0.1;
-    // Q(0,1) *= std::pow(dt,3) * 0.1;
-    // Q(1,0) *= 0.5 * std::pow(dt,3) * 0.1;
-
-    // Q(2,2) *= 0.1;
-    
-    // Measurement noise covariance
-    double range_std = 3;
-    MatrixXd R(1,1);
-    R.setIdentity();
-    R *= range_std*range_std;
-
-    // Kalman Filter
-    Ctra_EKF3D ekf;
-    ekf.reset(state0, P0, Q);
-
-    std::vector<double> track_x, track_vx, track_y, filter_estimate_x, filter_estimate_vx, filter_estimate_y, times;
-    StateVector state_temp;
-    std::vector<StateVector> track, filter_estimate;
-    Measurement z = MeasurementModel::h(state, dt);
-    JacobiMatrix H = MeasurementModel::H(state, dt);
-
-    int timesteps = 60.0/dt;
-    for (int i = 0; i < timesteps; i++)
-    {
-        times.push_back(i);
-        state_temp = MotionModel::simulate(state, dt);
-        track.push_back(state_temp);
-        track_x.push_back(state_temp[0]);
-        track_vx.push_back(state_temp[1]);
-        track_y.push_back(state_temp[2]);
-
-        z = MeasurementModel::h(state, dt);
-        H = MeasurementModel::H(state, dt);
-        ekf.observation_update(z, H, R, 1000000);
-        filter_estimate.push_back(ekf.get_state());
-        
-        state_temp = ekf.get_state();
-        filter_estimate_x.push_back(state_temp[0]);
-        filter_estimate_vx.push_back(state_temp[1]);
-        filter_estimate_y.push_back(state_temp[2]);
-        ekf.temporal_update(dt);
-    }
-    
-    // for (int i = 0; i < timesteps; i++)
-    // {
-    //     std::cout<< i <<"_1: "<< track.at(i).transpose() << "\n";
-    //     std::cout<< i <<"_2: "<< filter_estimate.at(i).transpose() << "\n";
-    // }
-    
-    // Set the size of output image to 1200x780 pixels
-    plt::figure_size(1200, 780);
-    // Plot line from given x and y data. Color is selected automatically.
-    plt::plot(times, track_y);
-    // Plot a red dashed line from given x and y data.
-    plt::plot(times, filter_estimate_y,"r--");
-    // Plot a line whose name will show up as "log(x)" in the legend.
-    // plt::named_plot("log(x)", x, z);
-    // Set x-axis to interval [0,1000000]
-    // plt::xlim(0, 1000*1000);
-    // Add graph title
-    plt::title("Sample figure");
-    // Enable legend.
-    plt::legend();
-    // Save the image (file format is determined by the extension)
-    plt::show();
-
-    std::cout<<"Real state is: \n"<< state << "\n";
-    std::cout<<"Estimated state is: \n"<< ekf.get_state() << "\n";
-
-    return 0;
-}
-
-// int main()
-// {
-//     using State = iav::state_predictor::filter::Ctrv_EKF2D::StateVector;
-//     using Matris = iav::state_predictor::filter::Ctrv_EKF2D::StateMatrix;
-
-//     std::cout<< "Filter: EKF"<<std::endl;
-
-//     iav::state_predictor::filter::Ctrv_EKF2D my_ekf;
-//     State x0;
-//     x0 << 1, 1 ,0, 0.5, 0.5, 0.5;
-//     Matris init_cov;
-//     init_cov.setIdentity()*1e-6;
-//     my_ekf.reset(x0, init_cov),
-
-//     std::cout<<"Your state is: \n"<< my_ekf.get_state() << "\n";
-//     std::cout<<"Your covariance is: \n"<< my_ekf.get_covariance() << "\n";
-// }
-
-void pass(const Matrix<double ,-1, -1>& m)
-{
-    std::cout<< "Rows: " << m.rows() <<std::endl;
-    std::cout<< "Cols: " << m.cols() <<std::endl;
-}
-int main2()
-{
-    Matrix<double,-1,-1> mym(2,12);
-    mym.setZero();
-    mym(0,0) = 14.0;
-    mym(0,3) = 14.0;
-    std::cout<< "Matrix: \n" << mym <<std::endl;
-    // pass(mym);
-    return 0;
-}
-
-int main4()
-{
-    return 1;
-    // main3();
-    // main2();
 }
